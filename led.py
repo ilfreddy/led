1c97b1d99a40e7ce89fbadf86335ffe20d8672a5
Luca Frediani <luca.frediani@gmail.com>
2020-01-26 17:12:57 +0100

    First led test

---
+++ led.py
@@ -0,0 +1,21 @@
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
+
+button = Button(2)
+
+
+#while True:
+#    if button.is_pressed:
+#        print("Pressed")
+#    else:
+#        print("Released")
+#    sleep(1)
+
